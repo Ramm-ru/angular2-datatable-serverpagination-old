6afdd23b741b128836e0d50e1f5d612329d2019e
Richard Vojtenko <richard.vojtenko@vistar.su>
2017-02-09 14:04:49 +0300

    Other Bug Fix

--- src/DataTable.ts
+++ src/DataTable.ts
@@ -78,10 +78,8 @@ export class DataTable implements OnChanges, DoCheck {
     public ngOnChanges(changes: {[key: string]: SimpleChange}): any {
         if (changes["inputData"]) {
             this.inputData = this.inputData || [];
-            this.onPageChange.emit({
-                activePage: this.activePage,
-                rowsOnPage: this.rowsOnPage,
-                dataLength: this.amountOfRows
+            this.onDataChange.emit({
+                length: this.inputData.length
             });
             this.mustRecalculateData = true;
         }
